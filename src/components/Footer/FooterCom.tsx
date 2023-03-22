326186b2a596c72d32082bcb0550dfc892baa456
Ahmet Faruk Uzunkaya <sosyal.ahmet.fuzunkaya@gmail.com>
2023-03-22 22:33:53 +0300

    finnnn

--- src/components/Footer/FooterCom.tsx
+++ src/components/Footer/FooterCom.tsx
@@ -1,6 +1,7 @@
 import { MDBFooter, MDBIcon, MDBContainer, MDBRow, MDBCol } from 'mdb-react-ui-kit';
 import React from 'react';
 import "./Footer.css";
+import { Link } from 'react-router-dom';
 
 export const FooterCom = () => {
     return (
@@ -27,18 +28,19 @@ export const FooterCom = () => {
                                 <p>16 Hardwicke Lane, Hardwicke Street, Dublin 1, DO1 Y223</p>
                             </MDBCol>
                             <MDBCol md="2" lg="2" xl="2" className='mx-auto mb-4'>
-                                <h6 className='text-uppercase fw-bold mb-4'>Products</h6>
-                                <p><a href='' className='text-reset'>Angular</a></p>
-                                <p><a href='' className='text-reset'>React  </a></p>
-                                <p><a href='' className='text-reset'>Vue    </a></p>
-                                <p><a href='' className='text-reset'>Laravel</a></p>
+                                <h6 className='text-uppercase fw-bold mb-4'>Donate</h6>
+                                <p><Link to='/Donate/Zekah' className='text-reset'>Zekah</Link></p>
+                                <p><Link to='/Donate/Charity' className='text-reset'>Charity</Link></p>
+                                <p><Link to='/Donate/Iftar' className='text-reset'>Iftar</Link></p>
+                                <p><Link to='/Donate/Qurbani' className='text-reset'>Qurbani</Link></p>
+                                <p><Link to='/Donate/Other' className='text-reset'>Other</Link></p>
                             </MDBCol>
                             <MDBCol md="3" lg="2" xl="2" className='mx-auto mb-4'>
-                                <h6 className='text-uppercase fw-bold mb-4'>Useful links</h6>
-                                <p><a href='' className='text-reset'>Pricing </a></p>
-                                <p><a href='' className='text-reset'>Settings</a></p>
-                                <p><a href='' className='text-reset'>Orders  </a></p>
-                                <p><a href='' className='text-reset'>Help    </a></p>
+                                <h6 className='text-uppercase fw-bold mb-4'>Useful Links</h6>
+                                <p><Link to='/FastingRamadan' className='text-reset'>Fasting</Link></p>
+                                <p><Link to='/PrayerIslam' className='text-reset'>Salah</Link></p>
+                                <p><Link to='/ZekahCharity' className='text-reset'>Zekah</Link></p>
+                                <p><Link to='/HajjUmrah' className='text-reset'>Hajj</Link></p>
                             </MDBCol>
                             <MDBCol md="4" lg="3" xl="3" className='mx-auto mb-md-0 mb-4'>
                                 <h6 className='text-uppercase fw-bold mb-4'>Contact</h6>
--- src/pages/Merriage.tsx
+++ src/pages/Merriage.tsx
@@ -1,49 +1,41 @@
-import { Button, Row } from 'antd';
+import { Row } from 'antd';
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import marr from "../assets/Files/marr.jpg";
 
 const Merriage = () => {
     return (
         <Container>
-            <div>
-                <div style={{
-                    
-                    backgroundPosition: 'center',
-                    backgroundSize: 'cover',
-                    backgroundRepeat: 'no-repeat',
-                    height: "500px",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    flexDirection: "column",
-                    borderRadius: "18px"
-                }}>
-                    <Row style={{
-                        color: "#C74300",
-                        fontSize: "36px",
+            <Container className="cont-another" style={{ marginBottom: "50px" }}>
+                <div>
+                    <div style={{
+                        backgroundImage: `url(${marr})`,
+                        backgroundPosition: 'center',
+                        backgroundSize: 'cover',
+                        backgroundRepeat: 'no-repeat',
+                        height: "500px",
                         display: "flex",
-                        justifyContent: "center",
-                        marginLeft: "12px",
-                        marginTop: "12px",
-                        fontFamily: "poppins-semibold, poppins, sans-serif",
-                        fontWeight: "700",
-                        letterSpacing: "-0.02em"
-                    }}>Islamic Marriage</Row>
-                     <Row>For details of the Nikah process at the Suleymaniye Mosque London, including how to apply. 079 3050 7610.</Row>
-                    <Row style={{
-                        marginTop: "20px",
-                        display: "flex",
-                        justifyContent: "center",
+                        justifyContent: "flex-start",
+                        alignItems: "center",
                         flexDirection: "column",
-                        alignItems: "center"
-                    }} gutter={[0, 32]}>
-                        <Button
-                            className='buttonVis'
-                            onClick={() => { }}
-                        ><a href="https://www.suleymaniyefunerals.com/en">Wisit Funeral WebSite</a></Button>
-                    </Row>
+                        borderRadius: "18px",
+                        marginTop: "20px"
+                    }}>
+                        <Row style={{
+                            color: "#136833",
+                            fontSize: "36px",
+                            display: "flex",
+                            justifyContent: "flex-start",
+                            marginLeft: "12px",
+                            marginTop: "36px",
+                            fontFamily: "helvetica-w01-bold,helvetica-w02-bold,helvetica-lt-w10-bold,sans-serif",
+                            fontWeight: "700",
+                            letterSpacing: "-0.02em"
+                        }}>Islamic Marriage</Row>
+                        <Row style={{ fontSize: "16px", marginTop: "20px", fontWeight: "550", backgroundColor: "#136833", padding: "8px", color: "white" }}>For details of the Nikah process at the Suleymaniye Mosque London, including how to apply. 079 3050 7610.</Row>
+                    </div>
                 </div>
-            </div>
+            </Container>
         </Container>
     );
 }
