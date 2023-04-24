9964f6ec88d75909f10cf07de5b7e57710517689
Murat Seçilmiş <muratse.dev@gmail.com>
2023-04-25 02:12:44 +0300

    init

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -6,7 +6,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 import './Navbar.css';
 import { NavDropdown } from 'react-bootstrap';
 import logo from '../../assets/logo.png'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function MyNavbar() {
     const Icon: ReactNode = <img src={logo} width={128} />;
@@ -19,15 +19,16 @@ function MyNavbar() {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto" />
                     <Nav>
-                        <Nav.Link><Link to = "/Home" >Home</Link></Nav.Link>
-                        <Nav.Link><Link to = "/Ramadan" >Ramadan</Link></Nav.Link>
+                        <Nav.Link><Link to="/Home" >Home</Link></Nav.Link>
+                         <Nav.Link><Link to="/Ramadan" >Ramadan</Link></Nav.Link>
+                {  /*     <NavDropdown.Item href="/Ramadan">Ramadan1</NavDropdown.Item>*/}
                         {/* <Nav.Link><Link to="/PrayerTime">PrayerTime</Link></Nav.Link> */}
                         <NavDropdown title="Imam Services">
-                            <Nav.Link><Link to = "/Chaplaincy" >Chaplaincy</Link></Nav.Link>
-                            <Nav.Link><Link to = "/Marriage" >Marriage</Link></Nav.Link>
-                            <Nav.Link><Link to = "/HelalFood" >Helal Food</Link></Nav.Link>
-                            <Nav.Link><Link to = "/HajjUmrah" >Hajj & Umrah</Link></Nav.Link>
-                            <Nav.Link><Link to = "/Education" >Education</Link></Nav.Link>
+                            <Nav.Link><Link to="/Chaplaincy" >Chaplaincy</Link></Nav.Link>
+                            <Nav.Link><Link to="/Marriage" >Marriage</Link></Nav.Link>
+                            <Nav.Link><Link to="/HelalFood" >Helal Food</Link></Nav.Link>
+                            <Nav.Link><Link to="/HajjUmrah" >Hajj & Umrah</Link></Nav.Link>
+                            <Nav.Link><Link to="/Education" >Education</Link></Nav.Link>
 
                             {/* <NavDropdown.Item href="/Chaplaincy">Chaplaincy</NavDropdown.Item>
                             <NavDropdown.Item href="/Merriage">Marriage</NavDropdown.Item>
@@ -36,11 +37,11 @@ function MyNavbar() {
                             <NavDropdown.Item href="/Education">Education</NavDropdown.Item> */}
                         </NavDropdown>
                         <NavDropdown title="Donate">
-                               <Nav.Link><Link to = "/Donate/Zakah" >Zakah</Link></Nav.Link>
-                               <Nav.Link><Link to = "/Donate/Sadaqa" >Sadaqa</Link></Nav.Link>
-                               <Nav.Link><Link to = "/Donate/Iftar" >Iftar</Link></Nav.Link>
-                               <Nav.Link><Link to = "/Donate/Qurbani" >Qurbani</Link></Nav.Link>
-                               <Nav.Link><Link to = "/Donate/Other" >Other</Link></Nav.Link>
+                            <Nav.Link><Link to="/Donate/Zakah" >Zakah</Link></Nav.Link>
+                            <Nav.Link><Link to="/Donate/Sadaqa" >Sadaqa</Link></Nav.Link>
+                            <Nav.Link><Link to="/Donate/Iftar" >Iftar</Link></Nav.Link>
+                            <Nav.Link><Link to="/Donate/Qurbani" >Qurbani</Link></Nav.Link>
+                            <Nav.Link><Link to="/Donate/Other" >Other</Link></Nav.Link>
 
                             {/* <NavDropdown.Item href="/Donate/Zakah">Zakah</NavDropdown.Item>
                             <NavDropdown.Item href="/Donate/Sadaqa">Sadaqa</NavDropdown.Item>
@@ -49,11 +50,11 @@ function MyNavbar() {
                             <NavDropdown.Item href="/Donate/Other">Other</NavDropdown.Item> */}
                         </NavDropdown>
                         <NavDropdown title="About Islam">
-                                
-                               <Nav.Link><Link to = "/AboutIslam" >Islam</Link></Nav.Link>
-                               <Nav.Link><Link to = "/BriefIntroduction" >Brief Introduction</Link></Nav.Link>
-                               <Nav.Link><Link to = "/PrayerIslam" >Prayer Islam</Link></Nav.Link>
-                          
+
+                            <Nav.Link><Link to="/AboutIslam" >Islam</Link></Nav.Link>
+                            <Nav.Link><Link to="/BriefIntroduction" >Brief Introduction</Link></Nav.Link>
+                            <Nav.Link><Link to="/PrayerIslam" >Prayer Islam</Link></Nav.Link>
+
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
