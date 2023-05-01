9a04dd38e41b41f0622fe9a9113b0c87fe8b6b80
Ahmet Faruk Uzunkaya <sosyal.ahmet.fuzunkaya@gmail.com>
2023-05-01 23:57:26 +0300

    editying slider

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -6,7 +6,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 import './Navbar.css';
 import { NavDropdown } from 'react-bootstrap';
 import logo from '../../assets/logo.png'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function MyNavbar() {
     const Icon: ReactNode = <img src={logo} width={128} />;
@@ -19,41 +19,43 @@ function MyNavbar() {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto" />
                     <Nav>
-                        <Nav.Link><Link to = "/Home" >Home</Link></Nav.Link>
-                        <Nav.Link><Link to = "/Ramadan" >Ramadan</Link></Nav.Link>
+                        <Nav.Link><Link to="/Home" >Home</Link></Nav.Link>
+                        <Nav.Link><Link to="/Ramadan" >Ramadan</Link></Nav.Link>
                         {/* <Nav.Link><Link to="/PrayerTime">PrayerTime</Link></Nav.Link> */}
                         <NavDropdown title="Imam Services">
-                            <Nav.Link><Link to = "/Chaplaincy" >Chaplaincy</Link></Nav.Link>
-                            <Nav.Link><Link to = "/Marriage" >Marriage</Link></Nav.Link>
-                            <Nav.Link><Link to = "/HelalFood" >Helal Food</Link></Nav.Link>
-                            <Nav.Link><Link to = "/HajjUmrah" >Hajj & Umrah</Link></Nav.Link>
-                            <Nav.Link><Link to = "/Education" >Education</Link></Nav.Link>
+                            <Nav.Link><Link to="/Chaplaincy" >Chaplaincy</Link></Nav.Link>
+                            <Nav.Link><Link to="/Marriage" >Marriage</Link></Nav.Link>
+                            <Nav.Link><Link to="/HelalFood" >Helal Food</Link></Nav.Link>
+                            <Nav.Link><Link to="/HajjUmrah" >Hajj & Umrah</Link></Nav.Link>
+                            <Nav.Link><Link to="/Education" >Education</Link></Nav.Link>
 
-                            {/* <NavDropdown.Item href="/Chaplaincy">Chaplaincy</NavDropdown.Item>
+                            {/* 
+                            <NavDropdown.Item href="/Chaplaincy">Chaplaincy</NavDropdown.Item>
                             <NavDropdown.Item href="/Merriage">Marriage</NavDropdown.Item>
                             <NavDropdown.Item href="/HelalFood">Helal Food</NavDropdown.Item>
                             <NavDropdown.Item href="/HajjUmrah">Hajj & Umrah</NavDropdown.Item>
-                            <NavDropdown.Item href="/Education">Education</NavDropdown.Item> */}
+                            <NavDropdown.Item href="/Education">Education</NavDropdown.Item> 
+                            */}
                         </NavDropdown>
                         <NavDropdown title="Donate">
-                               <Nav.Link><Link to = "/Donate/Zakah" >Zakah</Link></Nav.Link>
-                               <Nav.Link><Link to = "/Donate/Sadaqa" >Sadaqa</Link></Nav.Link>
-                               <Nav.Link><Link to = "/Donate/Iftar" >Iftar</Link></Nav.Link>
-                               <Nav.Link><Link to = "/Donate/Qurbani" >Qurbani</Link></Nav.Link>
-                               <Nav.Link><Link to = "/Donate/Other" >Other</Link></Nav.Link>
+                            <Nav.Link><Link to="/Donate/Zakah" >Zakah</Link></Nav.Link>
+                            <Nav.Link><Link to="/Donate/Sadaqa" >Sadaqa</Link></Nav.Link>
+                            <Nav.Link><Link to="/Donate/Iftar" >Iftar</Link></Nav.Link>
+                            <Nav.Link><Link to="/Donate/Qurbani" >Qurbani</Link></Nav.Link>
+                            <Nav.Link><Link to="/Donate/Other" >Other</Link></Nav.Link>
 
-                            {/* <NavDropdown.Item href="/Donate/Zakah">Zakah</NavDropdown.Item>
+                            {/* 
+                            <NavDropdown.Item href="/Donate/Zakah">Zakah</NavDropdown.Item>
                             <NavDropdown.Item href="/Donate/Sadaqa">Sadaqa</NavDropdown.Item>
                             <NavDropdown.Item href="/Donate/Iftar">Iftar</NavDropdown.Item>
                             <NavDropdown.Item href="/Donate/Qurbani">Qurbani</NavDropdown.Item>
-                            <NavDropdown.Item href="/Donate/Other">Other</NavDropdown.Item> */}
+                            <NavDropdown.Item href="/Donate/Other">Other</NavDropdown.Item> 
+                            */}
                         </NavDropdown>
                         <NavDropdown title="About Islam">
-                                
-                               <Nav.Link><Link to = "/AboutIslam" >Islam</Link></Nav.Link>
-                               <Nav.Link><Link to = "/BriefIntroduction" >Brief Introduction</Link></Nav.Link>
-                               <Nav.Link><Link to = "/PrayerIslam" >Prayer Islam</Link></Nav.Link>
-                          
+                            <Nav.Link><Link to="/AboutIslam" >Islam</Link></Nav.Link>
+                            <Nav.Link><Link to="/BriefIntroduction" >Brief Introduction</Link></Nav.Link>
+                            <Nav.Link><Link to="/PrayerIslam" >Prayer Islam</Link></Nav.Link>
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
--- src/components/ImgSlider/ImageSlider.tsx
+++ src/components/ImgSlider/ImageSlider.tsx
@@ -1,6 +1,6 @@
-import { Carousel } from 'react-carousel-minimal';
 import { Container } from 'react-bootstrap';
-import React, { useEffect, useState } from 'react';
+import ImageGallery from 'react-image-gallery';
+import React from 'react';
 import './ImageSlider.css';
 import P1 from '../../assets/Slider/Picture1.jpg';
 import P2 from '../../assets/Slider/Picture2.jpg';
@@ -14,52 +14,48 @@ import P9 from '../../assets/Slider/Picture9.jpg';
 import P10 from '../../assets/Slider/Picture10.jpg';
 import P11 from '../../assets/Slider/Picture11.jpg';
 
-interface ImageObject {
-    image: any;
-}
+const imageObjects = [
+    {
+        original: P1,
+        thumbnail: P1
+    },
+    {
+        original: P2,
+        thumbnail: P2
+    },
+    {
+        original: P3,
+        thumbnail: P3
+    },
+    {
+        original: P4,
+        thumbnail: P4
+    },
+    {
+        original: P5,
+        thumbnail: P5
+    },
+    {
+        original: P6,
+        thumbnail: P6
+    },
+    {
+        original: P7,
+        thumbnail: P7
+    }
+]
 
 export const ImageSlider = () => {
-    const [images, setImages] = useState<ImageObject[]>([]);
-    const [loading, setLoading] = useState<boolean>(false);
-
-    useEffect(() => {
-        setLoading(false);
-        let data: ImageObject[] = [];
-        let tempImages = [P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11];
-        tempImages.map(item => {
-            data.push({
-                image: item
-            });
-        });
-        setImages(data);
-        setLoading(true);
-    }, []);
-
     return (
-        <>
-            {loading &&
-                <Container>
-                    <Container className='cont-image-slide'>
-                        <Carousel
-                            data={images}
-                            time={2000}
-                            width="auto"
-                            height="500px"
-                            radius="36px"
-                            slideNumber={false}
-                            automatic={true}
-                            dots={true}
-                            pauseIconColor="white"
-                            pauseIconSize="40px"
-                            slideBackgroundColor="#136833"
-                            slideImageFit="cover"
-                            thumbnailWidth="100px"
-                            thumbnails={true}
-                            thumbnailsStyle
-                        />
-                    </Container>
-                </Container>
-            }
-        </>
+        <Container>
+            <Container className='cont-image-slide'>
+                <ImageGallery
+                    items={imageObjects}
+                    //lazyLoad
+                    infinite
+                    useTranslate3D={false}
+                />
+            </Container>
+        </Container>
     );
 }
